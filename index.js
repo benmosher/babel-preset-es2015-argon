340d824ea162e7fb2f5d19dddb6f2e12054d1498
Vikram Rangaraj <vik120@icloud.com>
2018-10-24 13:41:31 -0700

    babel 7 (#2)

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 
-module.exports = {
-  plugins: [
-    require("babel-plugin-transform-es2015-sticky-regex"),
-    require("babel-plugin-transform-es2015-unicode-regex"),
-    require("babel-plugin-transform-es2015-spread"),
-    require("babel-plugin-transform-es2015-parameters"),
-    require("babel-plugin-transform-es2015-destructuring"),
-    require("babel-plugin-transform-es2015-modules-commonjs"),
-  ]
+module.exports = function(api) {
+  api.assertVersion(7);
+  
+  return {
+    plugins: [
+      require("@babel/plugin-transform-sticky-regex"),
+      require("@babel/plugin-transform-unicode-regex"),
+      require("@babel/plugin-transform-spread"),
+      require("@babel/plugin-transform-parameters"),
+      require("@babel/plugin-transform-destructuring"),
+      require("@babel/plugin-transform-modules-commonjs"),
+    ]
+  };
 };
